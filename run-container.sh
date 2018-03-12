ffaeb4a5533d57ab2a937c3ba73727619761d84f
Alex Turner <aturner@tvpage.com>
2018-03-11 17:51:27 -0700

    Updated to use generic scripts and put better code in src/server.js

--- run-container.sh
+++ run-container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker container run -p 9999:8888 --rm --name node-app -v `pwd`/src:/usr/src/app/src node-app
+source env.sh
+
+docker container run -p 9999:8888 --rm --name ${CONTAINER_NAME} -v $(pwd)/src:/usr/src/app/src ${CONTAINER_NAME}
 
 
 
--- src/server.js
+++ src/server.js
@@ -11,6 +11,24 @@ let votes = {
     tacos: 0
 };
 
+let client = new pg.Client("postgres://postgres@172.17.0.2:5432/postgres");
+client.connect(function (err) {
+    if (err) throw err;
+
+    client.query("select option, votes from votes;", function (err, result) {
+        if (err) throw err;
+
+        result.rows.map(function(e, i) {
+            console.log(e);
+            switch (e.option) {
+                case "tacos" : votes.tacos = e.votes; break;
+                case "sandwiches" : votes.sandwiches = e.votes; break;
+            }
+        })
+    })
+});
+
+
 let urlencodedParser = bodyParser.urlencoded({extended:false});
 
 app.set('view engine', 'ejs');
@@ -22,7 +40,18 @@ app.get('/', function(req, res) {
     });
 });
 
-app.post('/vote', urlencodedParser, function(req, res) {
+function errThrower(err, res) { if (err) throw err; }
+function errLogger(err, res) { if (err) console.log(err.stack); }
+
+const updateTacosQuery = "update votes set votes = $1 where option = 'tacos'";
+const updateSandwicheQuery = "update votes set votes = $1 where option = 'sandwiches'";
+function updateDatabase(inVotes) {
+    client.query(updateTacosQuery, [inVotes.tacos], errLogger);
+    client.query(updateSandwicheQuery, [inVotes.sandwiches], errLogger);
+    console.log("Updated with " + inVotes);
+}
+
+app.post('/vote', urlencodedParser, (req, res) => {
     let vote = req.body.yourVote;
     if (vote === 'sandwiches') {
         votes.sandwiches = votes.sandwiches + 1;
@@ -33,6 +62,7 @@ app.post('/vote', urlencodedParser, function(req, res) {
     else {
         console.log("Somethign went wrong, value of vote wasn't any recognized elemnts, but was " + vote);
     }
+    updateDatabase(votes);
     res.redirect("/");
 });
 
